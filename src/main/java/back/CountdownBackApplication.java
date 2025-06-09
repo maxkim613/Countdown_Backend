7536bda66b300bc46740b64576f36388c93a0da2
vega14th <vega14th@naver.com>
2025-06-09 12:04:49 +0900

    Initial commit with Countdown_back project files

---
+++ src/main/java/back/CountdownBackApplication.java
@@ -0,0 +1,16 @@
+package back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableScheduling
+public class CountdownBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownBackApplication.class, args);
+	}
+
+}
