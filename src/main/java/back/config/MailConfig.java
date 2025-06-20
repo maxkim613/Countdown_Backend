7af425535f98b966a256ce77050435e7b455fa03
ahn <daybreak578@gmail.com>
2025-06-20 11:30:52 +0900

    이메일 인증

--- src/main/java/back/config/MailConfig.java
+++ src/main/java/back/config/MailConfig.java
@@ -14,15 +14,19 @@ public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com"); // 테스트용으로 Gmail 가능
+        mailSender.setHost("smtp.gmail.com"); 
         mailSender.setPort(587);
-        mailSender.setUsername("your_email@gmail.com");
-        mailSender.setPassword("your_password");
+        mailSender.setUsername("daybreak578@gmail.com");
+        mailSender.setPassword("uwra qirp floa srxt");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        
+        props.put("mail.smtp.ssl.trust", "*");
+
         props.put("mail.debug", "true");
 
         return mailSender;
